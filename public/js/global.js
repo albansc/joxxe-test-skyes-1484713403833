930c4e07a1f5dc854398568ff1ff80f8b6f8b362
joxxe274 <joxxe274@gmail.com>
2017-01-17 23:03:25 -0600

    primer commit

---
+++ public/js/global.js
@@ -0,0 +1,13 @@
+
+/* global ConversationPanel: true, PayloadPanel: true*/
+/* eslint no-unused-vars: "off" */
+
+// Other JS files required to be loaded first: apis.js, conversation.js, payload.js
+$(window).ready(function(){
+  (function() {
+    // Initialize all modules
+    ConversationPanel.init();
+    $('#textInput').focus();
+  })();  
+})
+
